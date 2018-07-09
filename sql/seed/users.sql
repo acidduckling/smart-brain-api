4def95030f16020027362bb12b891d419a8b3674
Your Name <acidduckling@gmail.com>
2018-07-09 12:42:28 +1000

    Add password hash for seeded user

--- sql/seed/users.sql
+++ sql/seed/users.sql
@@ -1,4 +1,4 @@
 BEGIN TRANSACTION;
-INSERT INTO users(name, email, entries, joined) values ('tim', 'tim@blah.com', '5', '2018-01-01');
-INSERT INTO login (hash, email) values ('hashhere', 'tim@blah.com');
+INSERT INTO users(name, email, entries, joined) values ('a', 'a@a.com', '5', '2018-01-01');
+INSERT INTO login (hash, email) values ('$2a$10$sNmzeOQNPQQbMTCLvJ9wN.KEtUXiQlOC62LJTBvRJ/IYyIOoNkW5K', 'a@a.com');
 COMMIT;
