586f62269bf951c72a0e58e91bcd0f919c24894f
Clayton Wakefield <acidduckling@gmail.com>
2018-07-24 18:19:58 +1000

    implement getAuthTokenId function

--- controllers/signin.js
+++ controllers/signin.js
@@ -29,8 +29,12 @@ const handleSignin = (db, bcrypt) => (req, res) => {
     .catch(err => Promise.reject('wrong credentials'));
 };
 
-const getAuthTokenId = () => {
-  console.log('auth ok');
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  redisClient.get(authorization, (err, response) => {
+    if (err || !response) return res.status(400).json('Unauthorized');
+    return res.json({ id: response });
+  });
 };
 
 const signToken = email => {
@@ -38,17 +42,23 @@ const signToken = email => {
   return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' });
 };
 
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
+};
+
 const createSessions = user => {
   // JWT Token, return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: true, userId: id, token };
+  return setToken(token, id)
+    .then(() => ({ success: true, userId: id, token }))
+    .catch(err => console.log(err));
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
-    ? getAuthTokenId()
+    ? getAuthTokenId(req, res)
     : handleSignin(db, bcrypt)(req, res)
         .then(data => {
           return data.id && data.email ? createSessions(data) : Promise.reject(data);
